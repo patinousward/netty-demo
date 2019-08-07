b5126c6db28869f9e34e2d063c5a4d22cf423732
jftang <patinousward@aliyun.com>
2019-08-07 14:21:26 +0800

    first commit

---
+++ netty-websoket-demo/src/main/java/com/patinousward/netty/demo/ChatServer.java
@@ -0,0 +1,60 @@
+package com.patinousward.netty.demo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by Administrator on 2019/8/6.
+ */
+public class ChatServer {
+    private final ChannelGroup channelGroup = new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE);
+    private final EventLoopGroup group = new NioEventLoopGroup();
+    private Channel channel;
+
+    public ChannelFuture start(InetSocketAddress address){
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(group).channel(NioServerSocketChannel.class).childHandler(createInitializer(channelGroup));
+        ChannelFuture future = serverBootstrap.bind(address);
+        future.syncUninterruptibly();  //wait绑定完成后
+        channel = future.channel();
+        return  future;
+    }
+
+    private ChannelHandler createInitializer(ChannelGroup channelGroup) {
+        return new ChatServerInitializer(channelGroup);
+    }
+
+    public void destory(){
+        if(channel != null) channel.close();
+        channelGroup.close();
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.err.println("please give port");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        final ChatServer endpoint = new ChatServer();
+        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                endpoint.destory();
+            }
+        });
+        //future.channel() 和endpoint.channel 是同一个对象
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
---
+++ netty-websoket-demo/src/main/java/com/patinousward/netty/demo/ChatServerInitializer.java
@@ -0,0 +1,33 @@
+package com.patinousward.netty.demo;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by Administrator on 2019/8/6.
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+
+    private final ChannelGroup channelGroup;
+
+    public ChatServerInitializer(ChannelGroup channelGroup){
+        this.channelGroup = channelGroup;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());//包括HttpRequestDecoder, HttpResponseEncoder
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler(channelGroup));
+    }
+}
