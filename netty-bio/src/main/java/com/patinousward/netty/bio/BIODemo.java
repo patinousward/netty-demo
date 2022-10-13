0568292629a53849b0be27d8606b09ff0edd1a27
patinousward <patinousward@163.com>
2022-10-13 16:04:59 +0800

    feat:write bio

--- netty-bio/src/main/java/com/patinousward/netty/bio/BIODemo.java
+++ netty-bio/src/main/java/com/patinousward/netty/bio/BIODemo.java
@@ -1,4 +1,48 @@
 package com.patinousward.netty.bio;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BIODemo {
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("server start");
+        while (true) {
+            System.out.println("wait to connect");
+            Socket socket = serverSocket.accept(); //block method
+            System.out.println("accept a connect");
+            executorService.execute(() -> {
+                handler(socket);
+            });
+        }
+    }
+
+    public static void handler(Socket socket) {
+        System.out.println(String.format("currentThreadId => %s,currentThreadName => %s", Thread.currentThread().getId(), Thread.currentThread().getName()));
+        try {
+            byte[] bytes = new byte[1024];
+            InputStream inputStream = socket.getInputStream();
+            while (true) {
+                System.out.println("wait to read");
+                int read = inputStream.read(bytes);// block method
+                if (read != -1) {
+                    System.out.println(new String(bytes, 0, read));
+                } else {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(socket);
+        }
+    }
 }
